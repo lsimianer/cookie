0cb4e3dc70576929caab008b1011ef8c5e9f9670
Lukas Simianer <lsimianer@gmail.com>
2020-07-18 22:24:18 -0500

    started

--- cookies/src/Home.js
+++ cookies/src/Home.js
@@ -6,10 +6,14 @@ import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
+import MenuIcon from '@material-ui/icons/Menu';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import { makeStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+
 import logo from './assets/logo.png'
+
 
 const useStyles = makeStyles({
   list: {
@@ -43,9 +47,21 @@ const Home = () => {
           </>
       )
     return (
-        <Grid container>            
+        <Grid container
+        // direction="column"
+        // alignItems="center"
+        // justify="center">
+        >
+          <Grid xs={6}>
+            <h3 className="navHeaderText"> FADED BY COOKIE</h3>
+          </Grid>  
+          <Grid xs={6}>
+            <IconButton>
+              <MenuIcon onClick={toggleDrawer}/>
+            </IconButton>
+          </Grid>          
             <Grid xs={12}>
-              <Button onClick={toggleDrawer}><img src={logo}/></Button>
+              <IconButton onClick={toggleDrawer}><img id="logoImgHead" src={logo}/></IconButton>
                 <Drawer 
                 className={classes.drawer}
                 width="75%"
@@ -54,6 +70,17 @@ const Home = () => {
                 onClose={toggleDrawer}>
                 {list}
               </Drawer>       
+            </Grid>
+            <Grid xs={12}>
+              <Button>Schedule Now</Button>
+              <Button>Media</Button>
+              <Button>Contact</Button>
+              <Button>Upcoming events</Button>              
+            </Grid>
+            <Grid xs={12}>
+              <div id="bodyDivOne">
+                this is the body bro
+              </div>
             </Grid>
         </Grid>
     );
